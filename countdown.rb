9926d9aef8fd0582ca76a478c3a8e8bed7a45feb
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-11 14:41:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(seconds)
 
-def countdown
+
+while seconds > 0
+puts "#{seconds} SECOND(S)!"
+ 
+seconds -= 1
 end
+return "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(x)
+  while x > 0
+  sleep(3.seconds)
+  x -= 1
+end
+
+end 
